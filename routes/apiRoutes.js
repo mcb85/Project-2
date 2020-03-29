60eacdfdd643350359bd77574cffe2ba503bc1ed
Jordan Kaiser <Jordan.kaiser@gmail.com>
2020-03-29 15:51:22 -0400

    completed user auth with passport and jwt

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,91 @@
 var db = require("../models");
+// const express = require("express");
+// const bodyParser = require('body-parser');
+const jwt = require("jsonwebtoken");
+// import passport and passport-jwt modules
+const passport = require("passport");
+const passportJWT = require("passport-jwt");
+// ExtractJwt to help extract the token
+let ExtractJwt = passportJWT.ExtractJwt;
+// JwtStrategy which is the strategy for the authentication
+let JwtStrategy = passportJWT.Strategy;
+let jwtOptions = {};
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+jwtOptions.secretOrKey = "wowow";
+// const createUser = async ({ name, password }) => {
+//   return await db.User.create({ name, password });
+// };
+// const getAllUsers = async () => {
+//   return await db.User.findAll();
+// };
+// const getUser = async obj => {
+//   return await db.User.findOne({
+//     where: obj
+//   });
+// };
+// lets create our strategy for web token
+// eslint-disable-next-line camelcase
+let strategy = new JwtStrategy(jwtOptions, function(jwt_payload, next) {
+  console.log("payload received", jwt_payload);
+  // eslint-disable-next-line camelcase
+  let user = getUser({ id: jwt_payload.id });
+  if (user) {
+    next(null, user);
+  } else {
+    next(null, false);
+  }
+});
+// use the strategy
+passport.use(strategy);
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  app.use(passport.initialize());
+  // get all users
+  app.get("/api/users/", function(req, res) {
+    db.User.findAll({}).then(function(dbUser) {
+      res.json(dbUser);
     });
   });
-
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  //get one user
+  app.get("/api/users/:user/", function(req, res) {
+    db.User.findAll({
+      where: {
+        id: req.params.user
+      }
+    }).then(function(dbUser) {
+      res.json(dbUser);
     });
   });
-
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+  // register route
+  app.post("/register", function(req, res) {
+    console.log(req.body);
+    db.User.create({
+      id: req.body.id,
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+    }).then(function(dbCreateUser) {
+      res.json(dbCreateUser);
     });
+  });
+  app.post("/login", async function(req, res) {
+    const { name, password } = req.body;
+    if (name && password) {
+      // we get the user with the name and save the resolved promise
+      // returned;
+      let user = await getUser({ name });
+      if (!user) {
+        res.status(401).json({ msg: "No such user found", user });
+      }
+      if (user.password === password) {
+        // from now on we’ll identify the user by the id and the id is
+        // the only personalized value that goes into our token
+        let payload = { id: user.id };
+        let token = jwt.sign(payload, jwtOptions.secretOrKey);
+        res.json({ msg: "ok", token: token });
+      } else {
+        res.status(401).json({ msg: "Password is incorrect" });
+      }
+    }
   });
 };
