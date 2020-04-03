1373594d249da52eb6d1655e1c4242ae406ea69f
Jordan Kaiser <jordan.kaiser@gmail.com>
2020-04-03 12:53:47 -0400

    fixed routes for calendar

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,12 +3,7 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    res.render("index")
   });
 
   app.get("/events", function(req, res){
