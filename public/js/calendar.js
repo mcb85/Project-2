9b29a88cedbff24eb5f378429803e4aa0463010d
Jordan Kaiser <Jordan.kaiser@gmail.com>
2020-04-03 19:20:20 -0400

    commented out calendar.js code since it isn't being used

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,14 +1,13 @@
-// import { Calendar } from '@fullcalendar/core';
-import dayGridPlugin from '@fullcalendar/daygrid';
+// // import { Calendar } from '@fullcalendar/core';
+// import dayGridPlugin from '@fullcalendar/daygrid';
 
-document.addEventListener('DOMContentLoaded', function() {
-  var calendarEl = document.getElementById('calendar');
+// document.addEventListener('DOMContentLoaded', function() {
+//   var calendarEl = document.getElementById('calendar');
 
-  var calendar = new Calendar(calendarEl, {
-    plugins: [ dayGridPlugin ],
-    defaultView: 'dayGridMonth'
-  });
+//   var calendar = new Calendar(calendarEl, {
+//     plugins: [ dayGridPlugin ],
+//     defaultView: 'dayGridMonth'
+//   });
 
-  calendar.render();
-});
-
+//   calendar.render();
+// });
