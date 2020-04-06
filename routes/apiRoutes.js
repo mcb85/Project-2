31fe76d87847d6b34a3fec8a47d428095153d68f
Jordan Kaiser <Jordan.kaiser@gmail.com>
2020-04-06 18:25:24 -0400

    fixed prettier errors

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,30 +2,30 @@ var db = require("../models");
 
 const passport = require("../config/passport");
 
-module.exports = function (app) {
+module.exports = function(app) {
   app.use(passport.initialize());
   // get all users
-  app.get("/api/blog/", function (req, res) {
-    db.Post.findAll({}).then(function (dbPost) {
+  app.get("/api/blog/", function(req, res) {
+    db.Post.findAll({}).then(function(dbPost) {
       res.json(dbPost);
     });
   });
   //get one user
-  app.get("/api/users/:user/", function (req, res) {
+  app.get("/api/users/:user/", function(req, res) {
     db.User.findAll({
       where: {
         id: req.params.user
       }
-    }).then(function (dbUser) {
+    }).then(function(dbUser) {
       res.json(dbUser);
     });
   });
   // register route
-  app.post("/api/blog", function (req, res) {
+  app.post("/api/blog", function(req, res) {
     db.Post.create({
       title: req.body.title,
       body: req.body.body
-    }).then(function (dbCreatePost) {
+    }).then(function(dbCreatePost) {
       res.json(dbCreatePost);
     });
   });
@@ -40,29 +40,30 @@ module.exports = function (app) {
     "/signup",
     passport.authenticate(
       "local-signup",
-      function (res) {
+      function(res) {
         console.log(res);
-      }, {
+      },
+      {
         successRedirect: "/blog",
         failureRedirect: "/"
       }
     )
   );
   // )
-  app.get("/api/events", function (req, res) {
-    db.Events.findAll({}).then(function (dbEvents) {
+  app.get("/api/events", function(req, res) {
+    db.Events.findAll({}).then(function(dbEvents) {
       res.json(dbEvents);
     });
   });
 
-  app.post("/api/events", function (req, res) {
+  app.post("/api/events", function(req, res) {
     db.Events.create({
       name: req.body.name,
       date: req.body.date,
       description: req.body.descrition,
       location: req.body.location
-    }).then(function (dbEvents) {
-      res.json(dbEvents)
+    }).then(function(dbEvents) {
+      res.json(dbEvents);
     });
-  })
+  });
 };
